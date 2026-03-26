ed58299a840ea171f35848bd581e2487cc3a1365
Bobmataplagas <alex.burgos2506@gmail.com>
2026-03-26 10:14:51 -0600

    Creé la gui y boton de hola desde el master

---
+++ src/GuiGit.java
@@ -0,0 +1,49 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class GuiGit extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiGit frame = new GuiGit();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GuiGit() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnMaster = new JButton("Hola desde el master");
+		btnMaster.setBounds(70, 89, 151, 23);
+		contentPane.add(btnMaster);
+		
+		
+		
+
+	}
+}
